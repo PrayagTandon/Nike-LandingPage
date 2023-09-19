92de088847ebfac49bf00397054708e17ae2e06a
Prayag Tandon <tandonprayag99@gmail.com>
2023-09-19 22:54:11 +0530

    Setting up the reusable button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ label, iconURL }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red hover:bg-red-500 transition-all hover:-translate-y-1 active:-translate-y-0.5">
       {label}
 
       <img
