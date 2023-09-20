4acce265ee2f511ba59c4c4c5a44284d1576fa5f
Prayag Tandon <tandonprayag99@gmail.com>
2023-09-20 10:42:56 +0530

    Completing the project

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,7 +11,9 @@ const Hero = () => {
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>Our Summer Collections</p>
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collections
+        </p>
 
         <h1>
           <span>The New Arrival</span>
