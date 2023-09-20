b0943a0d19f8d6b949f87a828b6992d37f227372
Prayag Tandon <tandonprayag99@gmail.com>
2023-09-20 10:58:28 +0530

    Finishing the Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -15,12 +15,16 @@ const Hero = () => {
           Our Summer Collections
         </p>
 
-        <h1>
-          <span>The New Arrival</span>
-          <span>Nike Shoes</span>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold flex flex-col flex-wrap">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
+          <span>
+            <span className="inline-block text-coral-red mt-3">Nike</span> Shoes
+          </span>
         </h1>
 
-        <p>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
