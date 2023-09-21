103eab7b0ed215cf0ed3dc28cafeebd8083f96df
Prayag Tandon <tandonprayag99@gmail.com>
2023-09-21 10:42:03 +0530

    Setting up the super quality section - which is similar to the hero section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,36 @@
+import { arrowRight } from "../assets/icons";
+
+import { Button } from "../components";
+
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collections
+        </p>
+
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold flex flex-col flex-wrap">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
+          <span>
+            <span className="inline-block text-coral-red mt-3">Nike</span> Shoes
+          </span>
+        </h1>
+
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+
+        <Button label="View Details" iconURL={arrowRight} />
+      </div>
+    </section>
+  )
 };
 
 export default SuperQuality;
