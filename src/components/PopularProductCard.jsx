e9cf8d38a123b2bdda14119e3efafa997d5bdf38
Prayag Tandon <tandonprayag99@gmail.com>
2023-09-21 10:29:49 +0530

    Completing the project

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -6,18 +6,20 @@ const PopularProductCard = ({ imgURL, name, price }) => {
             <img
                 src={imgURL}
                 alt={name}
-                className="w-[280px] h-[280px] "
+                className="w-[282px] h-[282px] "
             />
-            <div className="mt-8 flex justify-start items-center gap-2.5">
-                <img
-                    src={star}
-                    alt="rating"
-                    width={24}
-                    height={24}
-                />
-                <p className='font-montserrat text-xl leading-normal text-slate-gray'>(4.5)</p>
+            <div className="mt-8 flex sm:flex-col justify-start gap-2 max-sm:gap-4">
+                <div className='flex items-center gap-2 justify-start'>
+                    <img
+                        src={star}
+                        alt="rating icon"
+                        width={24}
+                        height={24}
+                    />
+                    <p className='font-montserrat text-xl leading-normal text-slate-gray'>(4.5)</p>
+                </div>
                 <h3 className='mt-2 text-xl leading-normal font-semibold font-palanquin'>{name}</h3>
-                <p className='mt-2 font-semibold font-montserrat text-coral-red leading-normal text-21x'>{price}</p>
+                <p className='mt-2 font-semibold font-montserrat text-coral-red leading-normal text-xl'>{price}</p>
             </div>
         </div>
     )
