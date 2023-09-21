6a4d7ae11b1a4ce9abf9cf617411f32a5e8737b2
Prayag Tandon <tandonprayag99@gmail.com>
2023-09-21 10:56:28 +0530

    Super quality section text done

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -9,18 +9,13 @@ const SuperQuality = () => {
       className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
     >
       <div className="flex flex-1 flex-col">
-        <p className="text-xl font-montserrat text-coral-red">
-          Our Summer Collections
-        </p>
 
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold flex flex-col flex-wrap">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
-            The New Arrival
-          </span>
-          <span>
-            <span className="inline-block text-coral-red mt-3">Nike</span> Shoes
-          </span>
-        </h1>
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You
+          <span className=" text-coral-red">Super</span>
+          <br />
+          <span className="inline-block text-coral-red mt-3">Quality</span> Shoes
+        </h2>
 
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylish Nike arrivals, quality comfort, and innovation for
@@ -29,7 +24,7 @@ const SuperQuality = () => {
 
         <Button label="View Details" iconURL={arrowRight} />
       </div>
-    </section>
+    </section >
   )
 };
 
