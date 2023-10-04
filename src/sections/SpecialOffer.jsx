1edda959dfe4b76ef6d712f5ebea4fff677263f1
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-04 23:29:21 +0530

    Reusing the previously created components

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,4 +1,6 @@
+import { arrowRight } from "../assets/icons";
 import { offer } from "../assets/images";
+import { Button } from "../components";
 
 
 const SpecialOffer = () => {
@@ -10,7 +12,29 @@ const SpecialOffer = () => {
           alt="Offers-Image"
           width={773}
           height={687}
+          className="object-contain w-full"
         />
+      </div>
+
+      <div className="flex flex-1 flex-col">
+
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You
+          <span className=" text-coral-red"> Super</span>
+          <span className=" text-coral-red"> Quality</span> Shoes
+        </h2>
+
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.
+        </p>
+
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+
+        <div className="mt-11">
+          <Button label="View Details" iconURL={arrowRight} />
+        </div>
       </div>
     </section>
   )
