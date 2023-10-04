e8ab3a8c013080db54a75cd9671bced2e6c9f27d
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-04 19:03:45 +0530

    Designing the services section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,15 +1,12 @@
-import { ServiceCard } from '../components'
-import { services } from '../constants'
+import { services } from "../constants";
+import { ServiceCard } from "../components";
 
 const Services = () => {
   return (
-    <section className="max-container flex justify-center flex-wrap gap-9">
+    <section className='max-container flex justify-center flex-wrap gap-9'>
       {services.map((service) => (
-        <ServiceCard key={service.label}
-          {...service}
-        />
+        <ServiceCard key={service.label} {...service} />
       ))}
-
     </section>
   );
 };
