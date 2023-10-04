b77195158e16350d28b4be5e4c6fe055c33010fe
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-05 00:19:16 +0530

    Dynamically assigning the second button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,19 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red hover:bg-red-500 transition-all hover:-translate-y-1 active:-translate-y-0.5">
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full hover:bg-red-500 transition-all hover:-translate-y-1 active:-translate-y-0.5
+    ${backgroundColor
+        ? `${backgroundColor} ${textColor} ${borderColor}`
+        : 'bg-coral-red text-white border-coral-red'}`}>
       {label}
 
-      {iconURL && <img
-        src={iconURL}
-        alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />}
-    </button>
+      {
+        iconURL && <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      }
+    </button >
   );
 };
 
