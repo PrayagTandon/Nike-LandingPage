d4846f63306c44ba02a0e4f4082d15b92aaa22e3
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-04 23:26:17 +0530

    Setting up the special offers section

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,5 +1,19 @@
+import { offer } from "../assets/images";
+
+
 const SpecialOffer = () => {
-  return <div>SpecialOffer</div>;
+  return (
+    <section className="flex justify-center flex-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img
+          src={offer}
+          alt="Offers-Image"
+          width={773}
+          height={687}
+        />
+      </div>
+    </section>
+  )
 };
 
 export default SpecialOffer;
