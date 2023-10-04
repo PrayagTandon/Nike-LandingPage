3b4c09924fe87624e1dc4b9d9116e65ef22a94a9
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-05 00:23:43 +0530

    Special offers section completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
   return (
-    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full hover:bg-red-500 transition-all hover:-translate-y-1 active:-translate-y-0.5
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full hover:bg-red-500 hover:text-white transition-all hover:-translate-y-1 active:-translate-y-0.5
     ${backgroundColor
         ? `${backgroundColor} ${textColor} ${borderColor}`
         : 'bg-coral-red text-white border-coral-red'}`}>
