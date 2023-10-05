bfad2d066e883be50c4a12414a423e43b3943501
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-05 12:23:51 +0530

    Subscribe section completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
-const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor, fullWidth }) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full hover:bg-red-500 hover:text-white transition-all hover:-translate-y-1 active:-translate-y-0.5
     ${backgroundColor
         ? `${backgroundColor} ${textColor} ${borderColor}`
-        : 'bg-coral-red text-white border-coral-red'}`}>
+        : 'bg-coral-red text-white border-coral-red'} ${fullWidth && 'w-full'}`}>
       {label}
 
       {
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,3 +1,5 @@
+import { Button } from "../components";
+
 const Subscribe = () => {
   return (
     <section
@@ -6,6 +8,15 @@ const Subscribe = () => {
       <h3 className="text-[40px] leading-[68px] lg:max-w-md font-palanquin font-bold ">Sign up for
         <span className="text-coral-red"> Updates </span>& Newsletter
       </h3>
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+        <input
+          type="text"
+          placeholder="subscribe@nike.com"
+          className="input" />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign up" fullWidth />
+        </div>
+      </div>
     </section>
   );
 };
