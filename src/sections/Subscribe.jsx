56c237b8f24c6415df7e8d0a10d24d0a18d65b2f
Prayag Tandon <tandonprayag99@gmail.com>
2023-10-05 12:14:55 +0530

    Setting up the subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,5 +1,13 @@
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      className="max-container flex justify-between items-center max-lg:flex-col gap-10"
+      id="contact-us">
+      <h3 className="text-[40px] leading-[68px] lg:max-w-md font-palanquin font-bold ">Sign up for
+        <span className="text-coral-red"> Updates </span>& Newsletter
+      </h3>
+    </section>
+  );
 };
 
 export default Subscribe;
